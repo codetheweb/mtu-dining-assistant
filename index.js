6cbf4f974dc2a3a5668e8832523830e97352ea9a
Max Isom <code@maxisom.me>
2018-12-05 10:53:54 -0500

    Cache meal plan for 1 hour

--- index.js
+++ index.js
@@ -1,125 +1,149 @@
-// Import the appropriate service and chosen wrappers
-const {
-  dialogflow,
-  Image,
-} = require('actions-on-google')
+// Import Google Action library
+const {dialogflow} = require('actions-on-google');
 
+// Import Express stuff
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Import helper packages
 const MTUDining = require('mtu-dining');
-const moment = require('moment');
+const Moment = require('moment');
 
-const Dining = new MTUDining();
+// Create instance
+const app = dialogflow();
+const Dining = new MTUDining(); // Helper instance for getting helper properties
 
-// Create an app instance
-const app = dialogflow()
+const mealPlan = {wads: new MTUDining(), mcnair: new MTUDining()};
 
-const express = require('express')
-const bodyParser = require('body-parser')
+// Update every hour
+function update() {
+  return Promise.all([mealPlan.wads.load(Dining.WADS), mealPlan.mcnair.load(Dining.MCNAIR)]);
+}
+
+setInterval(update, 3600000);
+
+// Load data in manually for first time
+(async () => {
+  await update();
+})();
 
 // Register handlers for Dialogflow intents
 app.intent('Get Menu', async conv => {
-	// Normalize parameters
-	let date = new moment();
-	let hall = Dining.MCNAIR;
+  // Normalize parameters
+  let date = new Moment();
+  let hall = Dining.MCNAIR;
 
-	if (conv.parameters.hall === 'Wads') {
-		hall = Dining.WADS;
-	}
+  if (conv.parameters.hall === 'Wads') {
+    hall = Dining.WADS;
+  }
 
-	if (conv.parameters.date !== '') {
-		date = new moment(conv.parameters.date);
-	}
+  if (conv.parameters.date !== '') {
+    date = new Moment(conv.parameters.date);
+  }
 
-	const queryDate = {month: date.month(), day: date.date()}
+  // If mealtime, change date to correct time during the day
+  switch (conv.parameters.meal) {
+    case 'breakfast':
+      date.set('hour', 9);
+      break;
+    case 'lunch':
+      date.set('hour', 12);
+      break;
+    case 'dinner':
+      date.set('hour', 17);
+      break;
+    default:
+      date.set('hour', 19);
+      break;
+  }
 
-	// Check if weekend
-	if (date.day() === 0 || date.day === 6) {
-		return conv.close('Go to Wads, you sluggard.');
-	}
+  const queryDate = {month: date.month(), day: date.date()};
 
-	// Load menu
-	await Dining.load(hall);
+  // Check if weekend
+  if (date.day() === 0 || date.day() === 6) {
+    return conv.close('Go to Wads, you sluggard.');
+  }
 
-	// Get menu
-	const thisMenu = Dining.get(queryDate);
+  // Get menu
+  const thisMenu = mealPlan[hall].get(queryDate);
 
-	// Capitalize hall
-	hall = hall[0].toUpperCase() + hall.slice(1);
+  // Capitalize hall
+  hall = hall[0].toUpperCase() + hall.slice(1);
 
-	// Build response
-  let response = hall;
+  // Build response
+  let response = 'On ';
+  response += date.format('MMMM Do') + ', ';
+
+  response += hall;
 
   // Correct verb form
-  const now = new moment();
+  const now = new Moment();
   if (now > date) {
     response += ' served ';
   } else {
     response += ' will serve ';
   }
 
-	if (conv.parameters.meal) {
-		response += arrToResponse({meal: conv.parameters.meal, menu: thisMenu});
-	} else {
-		response += arrToResponse({meal: 'all', menu: thisMenu});
-	}
+  if (conv.parameters.meal) {
+    response += arrToResponse({meal: conv.parameters.meal, menu: thisMenu});
+  } else {
+    response += arrToResponse({meal: 'all', menu: thisMenu});
+  }
 
-	return conv.close(response);
-})
+  return conv.close(response);
+});
 
 function arrToResponse(data) {
-	const meal = data.meal;
-	const menu = data.menu;
-	let thisMenu = {};
+  const {meal, menu} = data;
 
-	if (meal !== 'all') {
-		thisMenu[meal] = menu[meal];
-	} else {
-		thisMenu = menu;
-	}
+  let thisMenu = {};
 
-	let response = '';
+  if (meal === 'all') {
+    thisMenu = menu;
+  } else {
+    thisMenu[meal] = menu[meal];
+  }
+
+  let response = '';
 
   const len = Object.keys(thisMenu).length;
   let i = 0;
 
-	for (let thisMeal in thisMenu) {
-    i ++;
+  for (const thisMeal in thisMenu) {
+    if (Object.prototype.hasOwnProperty.call(thisMenu, thisMeal)) {
+      i++;
 
-    if (i === len && len !== 1) {
-      response += 'and ';
-    }
+      if (i === len && len !== 1) {
+        response += 'and ';
+      }
 
-		let items = thisMenu[thisMeal];
+      const items = thisMenu[thisMeal];
 
-		items[items.length - 1] = 'and ' + items[items.length - 1];
+      items[items.length - 1] = 'and ' + items[items.length - 1];
 
-		response += items.join(', ');
-		response += ' for ' + thisMeal;
+      response += items.join(', ');
+      response += ' for ' + thisMeal;
 
-		if (meal === 'all') {
-      if (i === len) {
-        response += '.';
+      if (meal === 'all') {
+        if (i === len) {
+          response += '.';
+        } else {
+          response += '; ';
+        }
       } else {
-        response += '; ';
+        response += '.';
       }
     }
-		else response += '.';
+  }
 
-	}
-
-	return response;
+  return response;
 }
 
-// Intent in Dialogflow called `Goodbye`
-app.intent('Goodbye', conv => {
-  conv.close('See you later!')
-})
+// Parse POST request as JSON
+const expressApp = express().use(bodyParser.json());
 
-app.intent('Default Fallback Intent', conv => {
-  conv.ask(`I didn't understand. Can you tell me something else?`)
-})
+// Add route
+expressApp.post('/fulfillment', app);
 
-const expressApp = express().use(bodyParser.json())
-
-expressApp.post('/fulfillment', app)
-
-expressApp.listen(3111)
+// Listen on port 3111
+expressApp.listen(3111);
